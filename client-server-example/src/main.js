24e0a9b65ba5d1a4fcd7a0f44775884ea936ac3d
JairNeto1 <jairmonteironeto@gmail.com>
2021-08-04 08:42:49 -0300

    updating the client-server folder

---
+++ client-server-example/src/main.js
@@ -0,0 +1,21 @@
+import { createServer } from 'http';
+
+const server = createServer({request, response} => {
+  switch (request.url) {
+    case '/status': { 
+      response.writeHead(200);
+      response.write('Okey');
+      response.end();
+      break;
+    }
+    default: { 
+      response.writeHead(402, 'Service not found.');
+      response.end();
+    }
+  }
+})
+
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Server is listening at http://127.0.0.1:8000')
+})
